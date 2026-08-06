96961d18d0023130db9efe9e2ef366da295392d8
Ghulam Mohy ud din <33268941+ghulam41@users.noreply.github.com>
2026-08-06 11:26:32 +1000

    add documentation setup

--- docs/make.jl
+++ docs/make.jl
@@ -4,7 +4,10 @@ makedocs(
     modules = [iPASA],
     sitename = "iPASA.jl",
     authors = "Biswajit Bala, Ghulam Mohy-ud-din",
-    format = Documenter.HTML(prettyurls = get(ENV, "CI", nothing) == "true"),
+    format = Documenter.HTML(
+        prettyurls = get(ENV, "CI", nothing) == "true",
+        canonical = "https://csiro-internal.github.io/ensys-iPASA.jl",
+    ),
     pages = [
         "Home" => "index.md",
         "API Reference" => "api.md",
@@ -12,4 +15,8 @@ makedocs(
     warnonly = true,
 )
 
-deploydocs(repo = "github.com/csiro-internal/ensys-iPASA.jl.git")
+deploydocs(
+    repo = "github.com/csiro-internal/ensys-iPASA.jl.git",
+    devbranch = "main",
+    push_preview = true,
+)
